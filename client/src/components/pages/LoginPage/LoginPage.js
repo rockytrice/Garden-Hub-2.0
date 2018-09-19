440422375ca61c89a71ff4d7205bfc3e307bb91e
Roderick Trice <rocky_trice@yahoo.com>
2018-09-19 00:36:19 -0400

    adding login page and testing route from signup to login

--- client/src/components/pages/LoginPage/LoginPage.js
+++ client/src/components/pages/LoginPage/LoginPage.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "./SignUp.css";
-// import LoginPage from "../LoginPage"
+import "./LoginPage.css";
 import { Container, Row, Col, Input, Button, Card, CardBody, } from 'mdbreact';
 
-class SignUp extends React.Component  {
+class LoginPage extends React.Component  {
   render() {
     return(
       <Container >
@@ -14,24 +13,21 @@ class SignUp extends React.Component  {
                <Card className="card-image" style={{backgroundColor: 'transparent'}}>
                  <div className="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
                    <div className="text-center">
-                     <h3 className="white-text mb-5 mt-4 font-weight-bold"><strong>SIGN</strong> <a className="green-text font-weight-bold"><strong> UP</strong></a></h3>
+                     <h3 className="white-text mb-5 mt-4 font-weight-bold"><strong>Login</strong> <a className="green-text font-weight-bold"><strong> In</strong></a></h3>
                    </div>
                    <Input label="Your email" group type="text" validate />
                    <Input label="Your password" group type="password" validate/>
                    <div className="md-form pb-3">
-                     <div className="form-check my-4">
-                       {/* <input className="form-check-input" type="checkbox" value="" id="defaultCheck17" />
-                       <label className="form-check-label white-text" htmlFor="defaultCheck17">Accept the<a href="/" className="green-text font-weight-bold"> Terms and   Conditions</a></label> */}
-                     </div>
+                    
                    </div>
                    <Row className="d-flex align-items-center mb-4">
                      <div className="text-center mb-3 col-md-12">
-                       <Button color="success" rounded type="button" className="btn-block z-depth-1">Sign Up</Button>
+                       <Button color="success" rounded type="button" className="btn-block z-depth-1">Login</Button>
                      </div>
                    </Row>
-                   <Col md="12">
+                   {/* <Col md="12">
                      <p className="font-small white-text d-flex justify-content-end">Have an account? <a href="/" className="green-text ml-1 font-weight-bold"> Log in</a></p>
-                   </Col>
+                   </Col> */}
                   </div>
                </Card>
               </CardBody>
@@ -43,11 +39,4 @@ class SignUp extends React.Component  {
   }
 };
 
-export default SignUp;
-
-
-
-
-// backgroundImage: 'url(https://techcrunch.com/wp-content/uploads/2018/04/flower-garden.jpg?w=730&crop=1)', width: '28rem', backgroundSize :'cover'
-
-// style={{marginLeft:'30%'}}
+export default LoginPage;
