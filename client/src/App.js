25270a693bd55904f2441ca9a0a2e96a843c1cc0
Roderick Trice <rocky_trice@yahoo.com>
2018-09-19 10:30:53 -0400

    login and signup complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-// import SignUp from "./components/pages/SignUp";
-import LoginPage from "./components/pages/LoginPage"
+import SignUp from "./components/pages/SignUp";
+// import LoginPage from "./components/pages/LoginPage"
 class App extends Component {
   render() {
     return (
       <div className="signup-form">
-     <LoginPage />
+      <SignUp />
+     {/* <LoginPage /> */}
       </div>
     );
   }
