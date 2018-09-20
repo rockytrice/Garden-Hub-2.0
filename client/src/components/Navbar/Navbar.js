0457deb753ea1a28ab6e778473136a8d4839c4e7
Roderick Trice <rocky_trice@yahoo.com>
2018-09-20 01:27:57 -0400

    inserted font icons and styled sidenavbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Input, Navbar, NavbarNav, NavItem, Fa, SideNavItem, SideNavCat, SideNavNav, SideNav,  } from 'mdbreact';
+import {  Navbar, NavbarNav, NavItem, Fa, SideNavItem, SideNavCat, SideNavNav, SideNav,  } from 'mdbreact';
 
 class DoubleNavigationPage extends React.Component {
     constructor(props) {
@@ -38,24 +38,31 @@ class DoubleNavigationPage extends React.Component {
         <Router>
         <div className="fixed-sn light-blue-skin">
             <SideNav
-            logo="https://mdbootstrap.com/img/logo/mdb-transparent.png"
             triggerOpening={this.state.toggleStateA}
             breakWidth={this.state.breakWidth}
             bg="https://i0.wp.com/meqasa.com/blog/wp-content/uploads/2016/03/garden-watering.jpg?ssl=1"
             mask="strong"
             >
-            
-            <Input type="text" default="Search" style={{ color: '#fff', padding: '8px 10px 8px 30px', boxSizing: 'border-box'}} />
             <SideNavNav>
-                <SideNavCat name="Submit blog" id="submit-blog-cat" icon="chevron-right">
+                <SideNavCat name="Dashboard" id="dash-board" icon="line-chart">
                     <SideNavItem>Submit listing</SideNavItem>
                     <SideNavItem>Registration form</SideNavItem>
                 </SideNavCat>
+                <SideNavCat name="Overview" id="full-app" icon="eye">
+                    <SideNavItem></SideNavItem>
+                </SideNavCat>    
                 <SideNavCat name="Instruction" id="instruction-cat" icon="hand-pointer-o">
-                    <SideNavItem>For bloggers</SideNavItem>
-                    <SideNavItem>For authors</SideNavItem>
+                    <SideNavItem>Install</SideNavItem>
                 </SideNavCat>
-                <SideNavCat name="About" id="about-cat" icon="eye">
+                <SideNavCat name="Control" id="controller" icon="toggle-on">
+                    <SideNavItem>Instruction</SideNavItem>
+                    <SideNavItem>Monthly meetings</SideNavItem>
+                </SideNavCat>    
+                <SideNavCat name="Units" id="temp" icon="thermometer-3">
+                    <SideNavItem>Instruction</SideNavItem>
+                    <SideNavItem>Monthly meetings</SideNavItem>  
+                </SideNavCat>      
+                <SideNavCat name="About" id="about-cat" icon="info-circle">
                     <SideNavItem>Instruction</SideNavItem>
                     <SideNavItem>Monthly meetings</SideNavItem>
                 </SideNavCat>
