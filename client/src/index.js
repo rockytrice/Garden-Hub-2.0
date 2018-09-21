b58d68d3298222bd458daea72e45aca21f6d98ba
Jonathan <sellsj14@gmail.com>
2018-09-21 14:37:36 -0400

    added the logo

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-// import Router from "./components/pages/Router";
-import App from "./App";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Login from './components/Login';
+import Register from './components/Register';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Router>
+        <div>
+          <Route exact path='/' component={App} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+  );
+  registerServiceWorker();
