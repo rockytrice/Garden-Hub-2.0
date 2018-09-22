c597275b112981bd101b975b128f2fe0ab83c48e
Jonathan <sellsj14@gmail.com>
2018-09-21 22:33:16 -0400

    finsihed updating the server with the necessary middleware to run the passsport-jwt

--- server.js
+++ server.js
@@ -22,6 +22,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'build')));
 
+//the middleware for the models
+app.use('/api/book', book);
+app.use('/api/auth', auth);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 const PORT = process.env.PORT || 3001;
 
