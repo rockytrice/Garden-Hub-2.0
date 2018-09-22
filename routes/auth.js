c8685debe30afa1f69f581b2882b34b786cc3159
Jonathan <sellsj14@gmail.com>
2018-09-21 22:01:14 -0400

    updated the single quotes to double quotes

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,15 @@
-var mongoose = require('mongoose');
-var passport = require('passport');
-var settings = require('../config/settings');
-require('../config/passport')(passport);
-var express = require('express');
-var jwt = require('jsonwebtoken');
+var mongoose = require("mongoose");
+var passport = require("passport");
+var settings = require("../config/settings");
+require("../config/passport")(passport);
+var express = require("express");
+var jwt = require("jsonwebtoken");
 var router = express.Router();
 var User = require("../models/user");
 
-router.post('/register', function(req, res) {
+router.post("/register", function(req, res) {
   if (!req.body.username || !req.body.password) {
-    res.json({success: false, msg: 'Please pass username and password.'});
+    res.json({success: false, msg: "Please pass username and password."});
   } else {
     var newUser = new User({
       username: req.body.username,
@@ -18,21 +18,21 @@ router.post('/register', function(req, res) {
     // save the user
     newUser.save(function(err) {
       if (err) {
-        return res.json({success: false, msg: 'Username already exists.'});
+        return res.json({success: false, msg: "Username already exists."});
       }
-      res.json({success: true, msg: 'Successful created new user.'});
+      res.json({success: true, msg: "Successful created new user."});
     });
   }
 });
 
-router.post('/login', function(req, res) {
+router.post("/login", function(req, res) {
   User.findOne({
     username: req.body.username
   }, function(err, user) {
     if (err) throw err;
 
     if (!user) {
-      res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
+      res.status(401).send({success: false, msg: "Authentication failed. User not found."});
     } else {
       // check if password matches
       user.comparePassword(req.body.password, function (err, isMatch) {
@@ -40,9 +40,9 @@ router.post('/login', function(req, res) {
           // if user is found and password is right create a token
           var token = jwt.sign(user.toJSON(), settings.secret);
           // return the information including token as JSON
-          res.json({success: true, token: 'JWT ' + token});
+          res.json({success: true, token: "JWT " + token});
         } else {
-          res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+          res.status(401).send({success: false, msg: "Authentication failed. Wrong password."});
         }
       });
     }
