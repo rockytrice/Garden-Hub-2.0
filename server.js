ea3c11cd3630e63d9f7d40cc2ce3ea2dd7345485
Jonathan <sellsj14@gmail.com>
2018-09-21 22:31:59 -0400

    finished the build of the react app

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+//require the models for the passport-jqt authentication
+var book = require("./routes/book");
+var auth = require("./routes/auth");
+//initialize express
 const app = express();
+
+//setup the mongoose connection
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/mern-secure', { promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+//setup the middleware for morgan, express, and bodyParser
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+const PORT = process.env.PORT || 3001;
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
