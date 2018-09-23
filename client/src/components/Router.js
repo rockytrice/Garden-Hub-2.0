9d89783d0cafd06918604808984d0f66b2093b92
Roderick Trice <rocky_trice@yahoo.com>
2018-09-22 21:08:12 -0400

    add files for the different irrigation zones. also cleaned up some css. Completed routes for navbar . All pages are switching nicely at the moment.

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,12 +2,16 @@ import React from "react";
 import {BrowserRouter,Route, Switch} from "react-router-dom";
 import Login from "./pages/SignIn/LoginPage";
 import SignUpPage from "./pages/SignUp/SignUp";
+import DashBoard from "./pages/DashboardPage/Dashboard";
+import InfoPage from "./pages/AboutPage/About";
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
         <Route exact path="/" component={SignUpPage} />
         <Route  path= "/Login" component = {Login} />
+        <Route path="/DashBoard" component= {DashBoard} />
+        <Route path="/About" component= {InfoPage} /> 
         </Switch>
     </BrowserRouter>
 )
--- client/src/components/pages/DashboardPage/Dashboard.js
+++ client/src/components/pages/DashboardPage/Dashboard.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Jumbotron, Container } from 'reactstrap';
 import "./Dashboard.css";
+import NavBar from "../../Navbar/Navbar";
 const DashBoard= ()=> {
     return(
+      <React.Fragment>
+      <NavBar />
         <Jumbotron fluid>
         <Container fluid style={{textAlign:"center"}} >
           <h1 className="display-3"style={{fontSize:25}} >Weather Forecast</h1>
           
         </Container>
       </Jumbotron> 
-      
+      </React.Fragment>
      
       
     );
