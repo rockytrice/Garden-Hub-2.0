6dfae856cf14e808c7366d3e62d0f8601dfd602b
Jonathan <sellsj14@gmail.com>
2018-09-24 16:15:34 -0400

    idk

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,9 @@ import SignUpPage from "./components/pages/SignUp"
 
 class App extends Component {
 
- /* constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    };
-  }
-  */
-
   componentDidMount() {
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken');
     axios.get('/DashBoard')
-      /*.then(res => {
-        this.setState({ books: res.data });
-        console.log(this.state.books);
-      })
-      .catch((error) => {
-        if(error.response.status === 401) {
-          this.props.history.push("/");
-        }
-      });*/
   }
   
 
@@ -42,39 +25,6 @@ class App extends Component {
       <div>
         <SignUpPage/>
       </div>
-      /*
-      <div class="container">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">
-              BOOK CATALOG &nbsp;
-              {localStorage.getItem('jwtToken') &&
-                <button class="btn btn-primary" onClick={this.logout}>Logout</button>
-              }
-            </h3>
-          </div>
-          <div class="panel-body">
-            <table class="table table-stripe">
-              <thead>
-                <tr>
-                  <th>ISBN</th>
-                  <th>Title</th>
-                  <th>Author</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.books.map(book =>
-                  <tr>
-                    
-                    <td>{book.title}</td>
-                    <td>{book.author}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>*/
     );
   }
 }
