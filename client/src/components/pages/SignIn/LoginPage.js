e9934dcd1c67fc71623e391895f44676fafe4476
Roderick Trice <rocky_trice@yahoo.com>
2018-09-23 21:06:22 -0400

    finally figured how to add images from local files.:)

--- client/src/components/pages/SignIn/LoginPage.js
+++ client/src/components/pages/SignIn/LoginPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Mask, Row, Col,  View, Container } from 'mdbreact';
 import CardLogin from "./LoginCard";
+import Irrigationicon from "../../../images/Irrigationicon.png";
 const LoginPage = () => (
   <div id="classicformpage">
         <View>
           <Mask className="d-flex justify-content-center align-items-center gradient">
             <Container>
               <Row>
-                <div className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5">
-                  <h1 className="h1-responsive font-weight-bold ">Welcome Back! </h1>
-                  <hr className="hr-light"/>
+                <div className="white-text text-center   text-md-left col-md-6 mt-xl-5 mb-5">
+                <img src={Irrigationicon} width="500" height="400" />
                 </div>
                 <Col md="6" xl="5" className="mb-4">
                   <CardLogin />
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
 import Router from "./components/Router";
+import "./index.css";
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
