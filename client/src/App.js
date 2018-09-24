9c91fa4d98e097fb7f9c4b7bc844f2c1d3966d8c
Jonathan <sellsj14@gmail.com>
2018-09-24 13:56:12 -0400

    still working on integrating the passport authentication

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       })
       .catch((error) => {
         if(error.response.status === 401) {
-          this.props.history.push("/login");
+          this.props.history.push("/");
         }
       });
   }
--- client/src/components/pages/SignIn/LoginPage.js
+++ client/src/components/pages/SignIn/LoginPage.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Mask, Row, Col,  View, Container } from 'mdbreact';
-import CardLogin from "./LoginCard";
+//import CardLogin from "./LoginCard";
+import {Card, CardBody, Input,Fa,Button} from "mdbreact";
+import SocialIcon from "./Icons"
 import Irrigationicon from "../../../images/Irrigationicon.png";
-const LoginPage = () => (
+class LoginPage extends Component (
+
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      message: ""
+    };
+  }
+  onChange = (e) => {
+    const state = this.state
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { username, password } = this.state;
+
+    axios.post("/api/auth/login", { username, password })
+      .then((result) => {
+        localStorage.setItem("jwtToken", result.data.token);
+        this.setState({ message: "" });
+        this.props.history.push("/")
+      })
+      .catch((error) => {
+        if(error.response.status === 401) {
+          this.setState({ message: "Login failed. Username or password not match" });
+        }
+      });
+  }
+
+
   <div id="classicformpage">
         <View>
           <Mask className="d-flex justify-content-center align-items-center gradient">
@@ -13,7 +49,19 @@ const LoginPage = () => (
                 <img src={Irrigationicon} width="500" height="400" />
                 </div>
                 <Col md="6" xl="5" className="mb-4">
-                  <CardLogin />
+                <Card id="classic-card" style={{marginTop:61}} >
+                  <CardBody className="z-depth-2 white-text">
+                  <h3 className="text-center"><Fa icon="user"/> Login:</h3>
+                    <hr className="hr-light"/>
+                    <Input label="Your email" icon="envelope"/>
+                    <Input label="Your password" icon="lock" type="password"/>
+                      <div className="text-center mt-4 black-text">
+                        <Button outline color="white">Login</Button>
+                        <hr className="hr-light"/>
+                          <SocialIcon />
+                      </div>
+                  </CardBody>
+                </Card>  
                 </Col>
               </Row>
             </Container>
