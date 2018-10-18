fdb57ed2338b8112c8084847827828f21f6ac357
Roderick Trice <rocky_trice@yahoo.com>
2018-10-18 15:57:39 -0400

    successfully made authenticated request in postman

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,14 @@
 const Authentication = require( "../controllers/Authentication");
+const passportService = require("../services/passport");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 
 module.exports = function(app){
+app.get("/", requireAuth, function(req, res){
+    res.send({ hi: "there" });
+})
 app.post("/signup",Authentication.signup)
 
 };
