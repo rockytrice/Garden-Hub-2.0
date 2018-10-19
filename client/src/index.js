5b9316f34b574bce481570f587bfb6dd334613ef
Roderick Trice <rocky_trice@yahoo.com>
2018-10-19 16:41:09 -0400

    wiring up middleware

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,24 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import App from "./App";
 import Router1 from "./components/Router";
 import { Provider } from "react-redux";
-import { createStore} from "redux";
-
+import { createStore, applyMiddleware} from "redux";
 import reducers from "./reducers"
+import reduxThunk from "redux-thunk";
+
+
 import "./index.css";
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
 
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+)
 
 ReactDOM.render(
-<Provider store={createStore(reducers, {})}>
+<Provider store={store}>
     <Router1>
         <App />
     </Router1>
